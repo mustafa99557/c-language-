71aa51403f98d912cadb0648d9da8729cb726aed
mustafa99557 <43775425+mustafa99557@users.noreply.github.com>
2025-09-28 20:11:13 +0530

    Create matrixadd.c

---
+++ matrixadd.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main (){
+	int m,n,j,i,first[10][10],second[10][10],sum[10][10];
+	printf("Enter no of rows, columns: ");
+	scanf("%d%d",&m,&n);
+	printf("Elements of matrix 1: ");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&first[i][j]);
+                    }
+	}
+	printf("Elements of matrix 2: ");
+	
+	for(i = 0;i<m;i++){
+		for(j = 0;j<n;j++){
+			scanf("%d",&second[i][j]);
+			              }
+	                  }
+	printf("Addition of matrices:\n");
+{
+		for(j=0;j<n;j++){
+			
+	
+	                    }
+		printf("\n");
+	}
+	
+	}
+	
