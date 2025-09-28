1955709e67cfcd186265d089cdc668732ae435ab
mustafa99557 <43775425+mustafa99557@users.noreply.github.com>
2025-09-28 20:19:07 +0530

    Create concatstrings.c

---
+++ concatstrings.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int i,j;
+	char s1[150],s2[150] ;
+	printf("string1 : ");
+	scanf("%s",s1);
+	printf("string2 : ");
+	scanf("%s",s2);
+	for()
+		{
+		i++;	
+		}
+	for(j=0;s2[j]!='\0'; j++)
+		{
+			
+		}
+
+	printf("concatenated string = %s\n",s1);
+	return 0;
+	
+}
