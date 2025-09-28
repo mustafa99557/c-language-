2ae580476d2a8f315ed15d737cee31ae999f2274
mustafa99557 <43775425+mustafa99557@users.noreply.github.com>
2025-09-28 20:34:56 +0530

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main ()
+{
+int n,i,j,temp,a[10];
+printf("n: ");
+scanf("%d",&n);
+printf("Elements: ");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+	}
+//before sorting
+
+for(i=0;i<n-1;i++)
+{
+for(j=0;j<n-i-1;j++)
+ {
+    if(a[j]>a[j+1])
+   {
+   
+ }
+ }
+}
+
+printf("After sorting: ");
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+
