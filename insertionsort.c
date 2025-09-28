e49412f033f754b3cb080adf667a8103f4b6aab3
mustafa99557 <43775425+mustafa99557@users.noreply.github.com>
2025-09-28 20:43:13 +0530

    Create insertionsort.c

---
+++ insertionsort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[20],i,n,j,temp;
+	printf("Enter no of elements: ");
+	scanf("%d", &n);
+	printf("Enter the elements: ");
+	for(i=0; i<n; i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Array before sort: ");
+	for(i=0;i<n;i++){
+	printf("%d ", a[i]);
+}
+
+	}
+	a[j]=temp;
+}
+printf("\nArray after insertion sort: ");
+for(i=0;i<n;i++){
+	
+}
+	return 0;
+}
